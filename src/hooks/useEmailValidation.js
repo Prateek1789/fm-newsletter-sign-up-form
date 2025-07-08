68d725f4091aa57cd40f6a1981c8d7a405df8339
PrateekP <prateekpanta1789@gmail.com>
2025-07-08 16:21:27 +0530

    Created custom hook for email validation state and error message

---
+++ src/hooks/useEmailValidation.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import Validator from '../utils/validator'
+
+const useEmailValidation = (email, delay = 1000) => {
+    const [validationState, setValidationState] = useState('idle');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    useEffect(() => {
+        if (!email) {
+            setValidationState('idle');
+            setErrorMessage('');
+            return;
+        }
+    
+        setValidationState('pending');
+    
+        const timer = setTimeout(() => {
+            const validationCheck = Validator.validateEmail(email);
+            setValidationState(validationCheck.isValid ? 'valid' : 'invalid');
+            setErrorMessage(validationCheck.message);
+        }, delay);
+    
+        return () => clearTimeout(timer);
+    }, [ email ]);
+
+   return { validationState, errorMessage } 
+}  
+
+export default useEmailValidation;
