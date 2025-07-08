6b2ba87a18c6de03b5c112bf2f9bf456dc252807
PrateekP <prateekpanta1789@gmail.com>
2025-07-08 22:58:33 +0530

    Added function to handle success modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react'
 import './App.css'
 import { Form } from './components/newsLetterForm'
 
 function App() {
+  const [userEmail, setUserEmail] = useState('');
+  const [modalState, setModalState] = useState(false);
+
+  const handleModal = (isValid, email) => {
+    setUserEmail(email);
+    setModalState(isValid);
+  }
+
   return (
     <>
-      <Form />
+      <Form onSubmit = { handleModal } />
     </>
   )
 }
