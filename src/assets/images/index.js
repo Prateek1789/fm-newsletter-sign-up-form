15a7cf1c3e101a03c95b12cbab384cf92fd588c4
PrateekP <prateekpanta1789@gmail.com>
2025-07-10 17:14:10 +0530

    Added object for images

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+import listIcon from './icon-list.svg'
+import listIconSuccess from './icon-success.svg'
+import bgMobile from './illustration-sign-up-mobile.svg'
+import bgTablet from './illustration-sign-up-tablet.svg'
+import bgDesktop from './illustration-sign-up-desktop.svg'
+
+const images = {
+    listIcon,
+    listIconSuccess,
+    bgMobile,
+    bgTablet,
+    bgDesktop
+}
+
+export default images
